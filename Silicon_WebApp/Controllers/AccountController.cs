87c7e205f1cec2e2146f45c83138a63ecd6a0505
sanna-hansson <sanna-hansson@live.com>
2024-05-04 16:34:02 +0200

    worked on account controller and changed name of type in address entity

--- Silicon_WebApp/Controllers/AccountController.cs
+++ Silicon_WebApp/Controllers/AccountController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Silicon_WebApp.ViewModels;
+using System.Security.Claims;
 
 namespace Silicon_WebApp.Controllers;
 
-public class AccountController : Controller
+[Authorize]
+public class AccountController(UserManager<UserEntity> userManager, ApplicationContext context) : Controller
 {
+    private readonly UserManager<UserEntity> _userManager = userManager;
+    private readonly ApplicationContext _context = context;
+
     public IActionResult Details()
     {
 
@@ -14,16 +24,88 @@ public class AccountController : Controller
     }
 
     [HttpPost]
-    public IActionResult UpdateBasicInfo()
+    public async Task<IActionResult> UpdateBasicInfo(AccountDetailsViewModel model)
     {
-        TempData["StatusMessage"] = "Unable to save the information.";
+        if (TryValidateModel(model.Basic!))
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user != null)
+            {
+                user.FirstName = user.FirstName;
+                user.LastName = user.LastName;
+                user.Email = user.Email;
+                user.PhoneNumber = user.PhoneNumber;
+                user.UserName = user.Email;
+                user.Bio = user.Bio;
+
+              var result = await _userManager.UpdateAsync(user);
+                if(result.Succeeded)
+                {
+                    TempData["StatusMessage"] = " Succesfully updated basic information.";
+                }
+                else
+                {
+                    TempData["StatusMessage"] = "Unable to save basic information.";
+                }
+            }
+        }
+        else
+        { 
+            TempData["StatusMessage"] = "Unable to save basic information.";
+        }
+       
         return RedirectToAction("Details", "Account");
     }
 
     [HttpPost]
-    public IActionResult UpdateAddressInfo()
+    public async Task<IActionResult> UpdateAddressInfo(AccountDetailsViewModel model)
     {
-        TempData["StatusMessage"] = "Unable to save the information.";
+        if (TryValidateModel(model.Address!))
+        {
+            var nameIdentifier = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+            var user = await _context.Users.Include(i => i.Address).FirstOrDefaultAsync(x => x.Id == nameIdentifier);
+
+          if (user != null)
+           {
+             if(user.Address!= null)
+                {
+                    user.Address.AddressLine_1 = model.Address!.AddressLine_1;
+                    user.Address.AddressLine_2 = model.Address!.AddressLine_2;
+                    user.Address.PostalCode = model.Address!.PostalCode;
+                    user.Address.City = model.Address!.City;
+                }
+                else
+                {
+                    user.Address = new AddressEntity
+                    {
+                        AddressLine_1 = model.Address!.AddressLine_1,
+                        AddressLine_2 = model.Address!.AddressLine_2,
+                        PostalCode = model.Address!.PostalCode,
+                        City = model.Address!.City
+
+                    };
+                }
+                
+             _context.Update(user);
+                await _context.SaveChangesAsync();
+
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    TempData["StatusMessage"] = " Succesfully updated address information.";
+                }
+                else
+                {
+                    TempData["StatusMessage"] = "Unable to save address information.";
+                }
+           }
+        }
+        else
+        {
+            TempData["StatusMessage"] = "Unable to save address information.";
+        }
+
         return RedirectToAction("Details", "Account");
     }
 }
