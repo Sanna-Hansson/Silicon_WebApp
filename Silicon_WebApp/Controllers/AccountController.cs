8fc48e0cf123bc14c863bac2c3166aac9eec786f
sanna-hansson <sanna-hansson@live.com>
2024-05-04 15:01:45 +0200

    created acccount details model and controller

---
+++ Silicon_WebApp/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Silicon_WebApp.ViewModels;
+
+namespace Silicon_WebApp.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Details()
+    {
+
+        var viewModel = new AccountDetailsViewModel();
+
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateBasicInfo()
+    {
+        TempData["StatusMessage"] = "Unable to save the information.";
+        return RedirectToAction("Details", "Account");
+    }
+
+    [HttpPost]
+    public IActionResult UpdateAddressInfo()
+    {
+        TempData["StatusMessage"] = "Unable to save the information.";
+        return RedirectToAction("Details", "Account");
+    }
+}
